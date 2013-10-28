699217e4fe2f4f80b26e9e214ac89c55033e3fde
少博 <@vagrant_home_pc>
2013-10-28 20:09:50 +0800

    update

--- BianLiDian/src/main/java/com/bianlidian/service/ItemsCategoryService.java
+++ BianLiDian/src/main/java/com/bianlidian/service/ItemsCategoryService.java
@@ -2,6 +2,7 @@ package com.bianlidian.service;
 
 import java.util.List;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,9 +15,16 @@ import com.bianlidian.model.ItemsCategory;
 public class ItemsCategoryService {
 	@Autowired
 	private ItemsCategoryDAO itemsCategoryDAO;
+	@Autowired
+	private RabbitTemplate template;
 
 	public List<ItemsCategory> findAll() {
 		return itemsCategoryDAO.findAll();
 	}
-	
+
+	@Transactional
+	public void test() {
+		template.convertAndSend("new order!!");
+	}
+
 }
